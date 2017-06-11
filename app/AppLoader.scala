f3b3c59dc4c1d1ad8492747fcd24b2aac067df43
Mike Slinn <mslinn@micronauticsresearch.com>
2017-06-11 07:52:56 -0700

    Not sure how to create tables using Quill, or even if that is possible

--- app/AppLoader.scala
+++ app/AppLoader.scala
@@ -1,5 +1,19 @@
-/**
-  * @author mslin_000 */
-class AppLoader {
+import play.api.{ApplicationLoader, LoggerConfigurator}
+import play.api.inject.guice.{GuiceApplicationBuilder, GuiceApplicationLoader}
 
+/** Bog standard Play Framework application loader that also creates the in-memory database. */
+class AppLoader extends GuiceApplicationLoader {
+  override def builder(context: ApplicationLoader.Context): GuiceApplicationBuilder = {
+
+    LoggerConfigurator(context.environment.classLoader).foreach {
+      _.configure(context.environment)
+    }
+
+    //models.dao.createDatabase(context.environment.getFile("conf/evolutions/default/1.sql"))
+
+    initialBuilder
+      .in(context.environment)
+      .loadConfig(context.initialConfiguration)
+      .overrides(overrides(context): _*)
+  }
 }
