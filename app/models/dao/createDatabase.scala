ff53a11bdb9237969f0876252d435427e4a28af4
Mike Slinn <mslinn@micronauticsresearch.com>
2017-06-11 07:52:37 -0700

    Not sure how to create tables using Quill, or even if that is possible

---
+++ app/models/dao/createDatabase.scala
@@ -0,0 +1,18 @@
+package models.dao
+
+import java.io.File
+import io.getquill.H2JdbcContext
+import models.User
+import scala.language.postfixOps
+
+object createDatabase {
+  lazy val ctx = new H2JdbcContext[TableNameSnakeCase]("quill")
+  import ctx._
+
+  def apply(file: File): Unit = {
+    val evolution = scala.io.Source.fromFile(file).mkString
+    println(evolution)
+    //run { quote { infix"${ lift(evolution) }".as[BatchAction[User]] } } // does not compile. How to run DDL?
+    ()
+  }
+}
